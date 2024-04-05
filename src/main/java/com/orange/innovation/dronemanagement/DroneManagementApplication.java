e88d42f29fe83a231fca72ca00ee923dcf7c87a0
Khaled Kassem <khaledkennedy777@gmail.com>
2024-04-05 20:44:49 +0200

    connect to Kafka

--- src/main/java/com/orange/innovation/dronemanagement/DroneManagementApplication.java
+++ src/main/java/com/orange/innovation/dronemanagement/DroneManagementApplication.java
@@ -1,7 +1,10 @@
 package com.orange.innovation.dronemanagement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+//import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class DroneManagementApplication {
@@ -10,4 +13,12 @@ public class DroneManagementApplication {
         SpringApplication.run(DroneManagementApplication.class, args);
     }
 
+//    @Bean
+//    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+//        System.out.println("Logged Kafka Produce Message");
+//        return args -> {
+//            kafkaTemplate.send("firstTopic", "Hello Kafka");
+//            kafkaTemplate.send("secondTopic","Hello Kafka");
+//        };
+//    }
 }
